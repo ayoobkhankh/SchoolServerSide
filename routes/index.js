fa25fd1cc56a713710de4f7e6f614905a0c449e7
Ayoob Khan <ayoobkhankh@outlook.com>
2018-06-09 19:30:36 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
+router.get('/', (req, res) => res.send('<h1>Va alaikumussalam.</h1>'))
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  // models.products.findAll().then(function (products) {
-  res.send('Hi');
-  // });
-});
+// router.get('/', function (req, res, next) {
+//   // models.products.findAll().then(function (products) {
+//   res.send('Hi');
+//   // });
+// });
 
 module.exports = router;
