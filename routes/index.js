41074bd0cbef69df9288445a6f54aca62a7bfa79
Ayoob Khan <ayoobkhankh@outlook.com>
2018-06-09 19:27:49 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   models.products.findAll().then(function (products) {
-    res.send(products);
+    res.send('Hi');
   });
 });
 
