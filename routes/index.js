bb8ba0618edd9831aa0a1b43f5e213c6792c8bdc
Ayoob Khan <ayoobkhankh@outlook.com>
2018-06-09 19:34:48 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../models');
+// var models = require('../models');
 
-router.get('/', (req, res) => res.send('<h1>Va alaikumussalam.</h1>'))
-/* GET home page. */
-// router.get('/', function (req, res, next) {
-//   // models.products.findAll().then(function (products) {
-//   res.send('Hi');
-//   // });
-// });
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
 
 module.exports = router;
+
+// router.get('/', (req, res) => res.send('<h1>Va alaikumussalam.</h1>'))
+// /* GET home page. */
+// // router.get('/', function (req, res, next) {
+// //   // models.products.findAll().then(function (products) {
+// //   res.send('Hi');
+// //   // });
+// // });
+
+// module.exports = router;
