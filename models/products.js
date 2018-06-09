d118db5fe94b4fe714cd90c2241238245f1d656d
Ayoob Khan <ayoobkhankh@outlook.com>
2018-06-09 19:04:04 +0530

    test

--- models/products.js
+++ models/products.js
@@ -1,9 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var products = sequelize.define('products', {
-    Name: DataTypes.STRING
+    Id: { type: DataTypes.STRING, primaryKey: true },
+    ProductName: DataTypes.STRING,
+    ProductType: DataTypes.STRING
   }, {});
-  products.associate = function(models) {
+  products.associate = function (models) {
     // associations can be defined here
   };
   return products;
