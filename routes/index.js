b76dda95c90e155e0a5ea14c432a992d69a87cb3
Ayoob Khan <ayoobkhankh@outlook.com>
2018-06-10 09:38:25 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,23 @@ var router = express.Router();
 var models = require('../models');
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function (req, res, next) {
+//   res.send('respond with a resource');
+// });
 
 module.exports = router;
 
 // router.get('/', (req, res) => res.send('<h1>Va alaikumussalam.</h1>'))
-// /* GET home page. */
-// // router.get('/', function (req, res, next) {
-// //   // models.products.findAll().then(function (products) {
-// //   res.send('Hi');
-// //   // });
-// // });
+/* GET home page. */
+router.post('/', function (req, res, next) {
+  // models.products.findAll().then(function (products) {
+  //   res.send(products);
+  // });
+
+  models.products.create({ ProductName: req.param('ProductName'), ProductType: req.param('ProductType') }).then(newproducts => {
+    res.json(newproducts);
+  })
+
+});
 
 // module.exports = router;
