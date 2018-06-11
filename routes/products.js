f26b7273b0f5836265ce309296672ac237467554
Ayoob Khan <ayoobkhankh@outlook.com>
2018-06-11 22:16:39 +0530

    test

--- routes/products.js
+++ routes/products.js
@@ -6,14 +6,30 @@ var models = require('../models');
 function verifyToken(req, res, next) {
     var bearerHeader = req.headers['authorization'];
     if (typeof bearerHeader !== undefined) {
-
+        try {
+            // const decode = jwt.verify(bearerHeader, 'secretkey')
+            const bearer = bearerHeader.split(' ');
+            const bearerToken = bearer[1];
+            req.token = bearerToken;
+            // console.log(bearerToken);
+            next()
+        } catch (err) {
+            res.sendStatus(403)
+        }
     } else {
         res.sendStatus(403)
     }
 }
 
 router.post('/tryjwt', verifyToken, (req, res, next) => {
-    res.send('It worked');
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if (err) {
+            res.sendStatus(403)
+        } else {
+            res.json({ message: 'Its done', authData });
+        }
+    })
+
 });
 
 router.get('/login', function (req, res, next) {
