cb7a34b0299e2759ffe4155e5605985d559f4289
Ayoob Khan <ayoobkhankh@outlook.com>
2018-06-21 13:39:27 +0530

    test

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(bodyParser)
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
