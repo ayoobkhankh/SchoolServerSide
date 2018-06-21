d4d17f4a720212112ab0755211c26fa7c5f8628d
Ayoob Khan <ayoobkhankh@outlook.com>
2018-06-21 09:18:08 +0530

    test

--- routes/shops.js
+++ routes/shops.js
@@ -3,13 +3,11 @@ var router = express.Router();
 var models = require('../models');
 
 router.get('/test', function (req, res, next) {
-    const user = {
-        id: 1,
-        usename: 'ayoob',
-        email: 'ayoob@gmail.com'
-    }
+    models.shops.findAll().then(function (shops) {
+        res.json({ worked: shops })
+    })
     // jwt.sign({ user: user }, 'secretkey', (err, token) => {
-    res.json({ worked: "token" })
+
 });
 
 module.exports = router;
