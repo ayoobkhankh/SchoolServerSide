069c12650449d4eb70db206b1720f6654dbd5b6b
Ayoob Khan <ayoobkhankh@outlook.com>
2018-06-22 07:41:23 +0530

    test

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 // app.use(bodyParser)
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/', indexRouter);
