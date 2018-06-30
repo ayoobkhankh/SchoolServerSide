04197be4bcb6e1fe536d30bf1198708a808dc226
Ayoob Khan <ayoobkhankh@outlook.com>
2018-06-30 19:43:59 +0530

    test

--- models/taxes.js
+++ models/taxes.js
@@ -2,10 +2,30 @@
 module.exports = (sequelize, DataTypes) => {
     var taxes = sequelize.define('taxes', {
         TaxName: DataTypes.STRING,
-        SGSTRate: DataTypes.DECIMAL,
-        CGSTRate: DataTypes.DECIMAL,
-        IGSTRate: DataTypes.DECIMAL,
-        CESSRate: DataTypes.DECIMAL
+        SGSTRate: {
+            type: DataTypes.DECIMAL(10, 2),
+            validate: {
+                isDecimal: true
+            }
+        },
+        CGSTRate: {
+            type: DataTypes.DECIMAL(10, 2),
+            validate: {
+                isDecimal: true
+            }
+        },
+        IGSTRate: {
+            type: DataTypes.DECIMAL(10, 2),
+            validate: {
+                isDecimal: true
+            }
+        },
+        CESSRate: {
+            type: DataTypes.DECIMAL(10, 2),
+            validate: {
+                isDecimal: true
+            }
+        },
     }, {});
     taxes.associate = function (models) {
         // associations can be defined here
