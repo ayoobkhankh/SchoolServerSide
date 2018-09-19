421b1f4734536ec5c250af869c304bf807412a7a
Ayoob Khan <ayoobkhankh@outlook.com>
2018-09-19 08:43:02 +0530

    test

--- models/users.js
+++ models/users.js
@@ -7,17 +7,30 @@ module.exports = (sequelize, DataTypes) => {
         email: { type: Sequelize.STRING, unique: true },
         password: DataTypes.STRING,
         token: Sequelize.STRING
-    }, {
-            freezeTableName: true,
-            instanceMethods: {
-                generateHash(password) {
-                    return bcrypt.hash(password, bcrypt.genSaltSync(8));
-                },
-                validPassword(password) {
-                    return bcrypt.compare(password, this.password);
-                }
-            }
+    }, {});
+
+    function cryptPassword(password, callback) {
+        bcrypt.genSalt(10, function (err, salt) { // Encrypt password using bycrpt module
+            if (err)
+                return callback(err);
+
+            bcrypt.hash(password, salt, function (err, hash) {
+                return callback(err, hash);
+            });
         });
+    }
+
+    users.beforeCreate(function (users, options, cb) {
+        debug('Info: ' + 'Storing the password');
+        cryptPassword(users.password, function (err, hash) {
+            if (err) return cb(err);
+            debug('Info: ' + 'getting ' + hash);
+
+            users.password = hash;
+            return cb(null, options);
+        });
+    });
+
     users.associate = function (models) {
         // associations can be defined here
     };
