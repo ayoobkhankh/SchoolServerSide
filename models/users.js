222587c9928ab8be43532cbc0563d58291542265
Ayoob Khan <ayoobkhankh@outlook.com>
2018-09-21 07:45:20 +0530

    .

--- models/users.js
+++ models/users.js
@@ -10,24 +10,15 @@ module.exports = (sequelize, DataTypes) => {
         password: DataTypes.STRING,
         token: Sequelize.STRING
     }, {
-            hooks: {
-                beforeCreate: function (users, next) {
-                    bcrypt.genSalt(11, function (err, salt) {
-                        if (err) console.log(err);
-                        bcrypt.hash(users.password, salt, function (err, result) {
-                            if (err) console.log(err);
-                            console.log(result);
-                            users.password = result;
-                            next();
-                        })
-                    })
-                }
-            }
         });
 
     users.associate = function (models) {
         // associations can be defined here
     };
+
+    users.hook('beforeCreate', (users, options) => {
+        users.password = 'patti';
+    });
 
     return users;
 };
