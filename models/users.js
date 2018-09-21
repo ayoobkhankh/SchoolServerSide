f71bf4ae993ac219ae95b29744b44455705210c5
Ayoob Khan <ayoobkhankh@outlook.com>
2018-09-21 19:22:26 +0530

    .

--- models/users.js
+++ models/users.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Sequelize = require('sequelize');
+var bcrypt = require('bcrypt');
 
 module.exports = (sequelize, DataTypes) => {
     var users = sequelize.define('users', {
@@ -17,25 +18,29 @@ module.exports = (sequelize, DataTypes) => {
 
     users.hook('beforeCreate', (users, options) => {
         var password = users.password;
+        var saltRounds = 10;
         console.log(password)
-        // var hashedpassword = hashpassword(password)
-        // users.password = hashedpassword;
-
-
-
-    });
-
-    async function hashpassword(input) {
-        const password = input;
-        const saltRounds = 10;
-
-        await bcrypt
+        bcrypt
             .hash(password, saltRounds)
             .then(hashedPassword => {
                 console.log("hash", hashedPassword);
-                return hashedPassword;
+                users.password = hashedPassword;
             })
-    }
+        // var hashedpassword = hashpassword(password)
+        // users.password = hashedpassword;
+    });
+
+    // async function hashpassword(input) {
+    //     const password = input;
+    //     const saltRounds = 10;
+
+    //     await bcrypt
+    //         .hash(password, saltRounds)
+    //         .then(hashedPassword => {
+    //             console.log("hash", hashedPassword);
+    //             return hashedPassword;
+    //         })
+    // }
 
     return users;
 };
