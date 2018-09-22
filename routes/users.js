67beadae9e8dd2ab93f7d4356ed179321a5343fe
Ayoob Khan <ayoobkhankh@outlook.com>
2018-09-22 18:49:52 +0530

    .

--- routes/users.js
+++ routes/users.js
@@ -28,7 +28,7 @@ router.post('/login', function (req, res, next) {
     // if (result.rows.length === 0) {
     //   return res.json({ message: "Invalid Username" });
     // }
-    res.json(result.rows.length);
+    res.json(result);
   })
 })
 // if (foundUser.rows.length === 0) {
