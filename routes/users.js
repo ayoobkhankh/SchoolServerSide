7316e5d12d56c096f19da95cc1ec971624197a50
Ayoob Khan <ayoobkhankh@outlook.com>
2018-09-22 18:36:06 +0530

    .

--- routes/users.js
+++ routes/users.js
@@ -20,26 +20,30 @@ router.post('/create', function (req, res, next) {
 });
 
 router.post('/login', function (req, res, next) {
-  try {
-    const foundUser = models.users.findOne({
-      where: { username: req.param('username') },
-    })
-    // if (foundUser.rows.length === 0) {
-    //   return res.json({ message: "Invalid Username" });
-    // }
-    // const hashedPassword = bcrypt.compare(
-    //   req.params('username'),
-    //   foundUser.rows[0].password
-    // );
-    // if (hashedPassword === false) {
-    //   return res.json({ message: "Invalid Password" });
-    // }
-    console.log(foundUser);
-    return res.json({ message: "foundUser" });
-    // return res.json({ message: "Logged In!" });
-  } catch (e) {
-    return res.json(e);
-  }
+
+  models.users.findOne({
+    where: {
+      username: req.params('username').then(function (result) {
+        res.json(result)
+      })
+    },
+  })
+  // if (foundUser.rows.length === 0) {
+  //   return res.json({ message: "Invalid Username" });
+  // }
+  // const hashedPassword = bcrypt.compare(
+  //   req.params('username'),
+  //   foundUser.rows[0].password
+  // );
+  // if (hashedPassword === false) {
+  //   return res.json({ message: "Invalid Password" });
+  // }
+  //   console.log(foundUser);
+  //   return res.json({ message: "foundUser" });
+  //   // return res.json({ message: "Logged In!" });
+  // } catch (e) {
+  //   return res.json(e);
+  // }
 });
 
 module.exports = router;
