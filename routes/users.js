46422973c3b9dcd672e59de58ad0ba30f2930077
Ayoob Khan <ayoobkhankh@outlook.com>
2018-09-22 18:25:28 +0530

    .

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,10 @@ var bcrypt = require('bcrypt');
 /* GET users listing. */
 router.post('/create', function (req, res, next) {
   models.users.create({
-    username: req.param('username'),
-    email: req.param('email'),
-    password: req.param('password'),
-    token: req.param('token')
+    username: req.params('username'),
+    email: req.params('email'),
+    password: req.params('password'),
+    token: req.params('token')
   }, {
       individualHooks: true
     }).then(function (users) {
@@ -22,14 +22,14 @@ router.post('/create', function (req, res, next) {
 router.post('/login', function (req, res, next) {
   try {
     const foundUser = models.users.findOne({
-      where: { username: req.param('username') },
+      where: { username: req.params('username') },
     })
     console.log(foundUser);
     if (foundUser.rows.length === 0) {
       return res.json({ message: "Invalid Username" });
     }
     const hashedPassword = bcrypt.compare(
-      req.param('username'),
+      req.params('username'),
       foundUser.rows[0].password
     );
     if (hashedPassword === false) {
