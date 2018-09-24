5f02002a81f1a58976b10cbead4ff020c9788fc9
Ayoob Khan <ayoobkhankh@outlook.com>
2018-09-24 07:11:50 +0530

    .

--- routes/users.js
+++ routes/users.js
@@ -15,6 +15,7 @@ router.post('/create', function (req, res, next) {
     }).then(function (users) {
       res.json({ "message": "Customer details saved" })
     }).catch(function (err) {
+      console.log(err);
       res.json({ "message": "An error occured!" })
     })
 });
